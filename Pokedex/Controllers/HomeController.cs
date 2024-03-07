ea28bb0234f61f9e102833253087586bbe663181
AmandaTasca <amandatasca012@gmail.com>
2024-03-07 14:41:18 -0300

    alterações salvas

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Pokedex.Models;
 
@@ -15,7 +16,14 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        List<Pokemon> pokemons = new ();
+        using(StreamReader leitor =new("Data\\pokemons.json")) 
+        {
+           string dados = leitor.ReadToEnd();
+           pokemons = JsonSerializer.Deserialize<List<Pokemons>>(dados);
+        }
+
+        return View(pokemons);
     }
 
     public IActionResult Privacy()
